e895b79f9536c19ec860cb6f6fda9e73d836577b
rusane <43827639+rusane@users.noreply.github.com>
2019-03-28 14:23:23 +0100

    Wrap up preprocessing

--- preprocessing.R
+++ preprocessing.R
@@ -1,4 +1,5 @@
 library(sm)
+library(MASS)
 
 # Read file
 data <- read.table("german.data.txt", sep="", header=FALSE, comment.char="")
@@ -21,27 +22,27 @@ data$credit_risk = factor(data$credit_risk) # 1=good, 2=bad
 # A95 : female : single
 levels(data$personal_status_sex) # 4 levels A91, A92, A93, A94 (i.e. no A95 = single females)
 
-A91_id <- which(data$personal_status_sex %in% 'A91')
-A92_id <- which(data$personal_status_sex %in% 'A92')
-A93_id <- which(data$personal_status_sex %in% 'A93')
-A94_id <- which(data$personal_status_sex %in% 'A94')
-male_id <- c(A91_id, A93_id, A94_id)
+A91_idx <- which(data$personal_status_sex %in% 'A91')
+A92_idx <- which(data$personal_status_sex %in% 'A92')
+A93_idx <- which(data$personal_status_sex %in% 'A93')
+A94_idx <- which(data$personal_status_sex %in% 'A94')
+male_idx <- c(A91_idx, A93_idx, A94_idx)
 
 data$sex <- NA
-data$sex[male_id] <- 'M'
-data$sex[-male_id] <- 'F'
+data$sex[male_idx] <- 'M'
+data$sex[-male_idx] <- 'F'
 data$sex <- factor(data$sex)
 
 # Function that splits data into balanced train and test set
 splitData <- function(data, x) {
   good_credit_risk <- which(data$credit_risk %in% 1)
   bad_credit_risk <- which(data$credit_risk %in% 2)
-  train_good_id <- sample(good_credit_risk, x*length(good_credit_risk))
-  train_bad_id <- sample(bad_credit_risk, x*length(bad_credit_risk))
-  train_id <- sort(c(train_good_id, train_bad_id))
-  train <- data[train_id,]
-  test <- data[-train_id,]
-  split <- list(data[train_id,], data[-train_id,])
+  train_good_idx <- sample(good_credit_risk, x*length(good_credit_risk))
+  train_bad_idx <- sample(bad_credit_risk, x*length(bad_credit_risk))
+  train_idx <- sort(c(train_good_idx, train_bad_idx))
+  train <- data[train_idx,]
+  test <- data[-train_idx,]
+  split <- list(data[train_idx,], data[-train_idx,])
   names(split) <- c("train", "test")
   return(split)
 }
@@ -51,46 +52,61 @@ train <- splitData(data, 0.8)$train
 test <- splitData(data, 0.8)$test
 
 
-# Create counterfactual data set
-data_CF <- data
-data_CF$sex <- NA
-data_CF$sex[male_id] <- 'F'
-data_CF$sex[-male_id] <- 'M'
-data_CF$sex <- factor(data$sex)
+# Create counterfactual data set (do not do it like this, use casual model to generate data and counterfactual data)
+train_CF <- train
+male_train_idx <- which(train_CF$sex %in% 'M')
+train_CF$sex[male_train_idx] <- 'F'
+train_CF$sex[-male_train_idx] <- 'M'
+test_CF <- test
+male_test_idx <- which(test_CF$sex %in% 'M')
+test_CF$sex[male_test_idx] <- 'F'
+test_CF$sex[-male_test_idx] <- 'M'
 
-train_CF <- splitData(data_CF, 0.8)$train
-test_CF <- splitData(data_CF, 0.8)$test
+train <- train_CF
+test <- test_CF
 
 
 # Model from paper Path-specific counterfactual fairness
 model <- glm(credit_risk ~ status + savings + housing + amount + duration + age + sex, family=binomial("logit"), data=train)
 summary(model)
 
+model <- glm(credit_risk ~ status + sex + age + foreign_worker + purpose, family=binomial("logit"), data=train)
+summary(model)
 
 
-# Model selection
-# model <- glm(credit_risk ~ . - personal_status_sex, family=binomial("logit"), data=train)
-# summary(model)
-# 
-# drop1(model, test='Chisq')
+# Model selection to find best fit (not the objective of this study...)
+model <- glm(credit_risk ~ . - personal_status_sex, family=binomial("logit"), data=train)
+summary(model)
+
+model.step <- stepAIC(model, direction="backward", trace = FALSE)
+model.step$anova
+
+model <- model.step
+summary(model)
+
+# mdrop1(model, test='Chisq')
 # model <- glm(credit_risk ~ . - job - housing - personal_status_sex, family=binomial("logit"), data=train)
 # summary(model)
-
 
 
 # Prediction
 predictions_raw <- predict(model, newdata=test, type='response')
 
-sm.density.compare(predictions_raw, test$sex, xlab="Credit risk probability", model="equal")
+sm.density.compare(predictions_raw, test$sex, xlab="Credit risk probability")
 title("Density plot comparison of sex")
+legend("topright", levels(test$sex), fill=2+(0:nlevels(test$sex)))
 
 predictions <- ifelse(predictions_raw > 0.5, 2, 1)
 error <- mean(predictions != test$credit_risk)
 print(paste('Accuracy:', 1-error))
 
 
-
 # Miscellaneous code
-d <- density(predictions_raw)
+d <- density(predictions_raw[male_idx], na.rm=TRUE)
 plot(d, main="Density plot of predicted credit risk")
 polygon(d, col="grey", border="black") 
+
+d2 <- density(predictions_raw[-male_idx], na.rm=TRUE)
+lines(d2)
+polygon(d2, col="white", border="black") 
+
